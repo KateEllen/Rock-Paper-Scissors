82d4d92b5354b2330771b488f1c54fd39ff36bf0
KateEllen <katemaher00@gmail.com>
2021-10-02 20:19:10 +0000

    Add colorama style to run.py

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
-import random
-import gspread
 from google.oauth2.service_account import Credentials
+import gspread
+import random
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
+
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -29,15 +33,15 @@ class Game:
         self.computer_option = None
 
     def rules(self):
-        print("Game Rules\n")
+        print(Fore.MAGENTA + Back.WHITE + "Game Rules")
         print("Enter 'R' for Rock")
         print("Enter 'P' for Paper")
         print("Enter 'S' for Scissors")
         print("If preferred, you can type the full word.")
         print("Rock beats Scissors")
         print("Scissors beats Paper")
-        print("Paper beats Rock")
-        print("Pick Rock, paper or scissors")
+        print("Paper beats Rock\n")
+        print(Fore.MAGENTA + Back.WHITE + "Now it's time to play!")
         self.play()
     # View most addicted player
 
@@ -52,7 +56,7 @@ class Game:
             self.user_choice = "s"
         else:
             print(
-                "Uh Oh, I don't think you've played this game before. Please try again.")  # noqa
+               Fore.RED + "Uh Oh, I don't think you've played this game before. Please try again.")  # noqa
             self.get_user_choice
 
     def get_computer_option(self):
@@ -66,8 +70,8 @@ class Game:
 
     def display_match_results(self):
         print("")
-        print("Player wins: " + str(self.player_wins))
-        print("Computer wins: " + str(self.comp_wins))
+        print(Fore.YELLOW + "Player wins: " + str(self.player_wins))
+        print(Fore.BLUE + "Computer wins: " + str(self.comp_wins))
         print("")
 
     def who_won(self):
@@ -75,48 +79,47 @@ class Game:
         if self.user_choice == "r":
             if self.computer_option == "r":
                 print(
-                    "You chose rock. The computer chose rock too. Congrats, you tied.")  # noqa
+                    Fore.YELLOW + "You chose rock. The computer chose rock too. Congrats, you tied.")  # noqa
 
             elif self.computer_option == "p":
-                print("You chose rock. The computer chose paper. Oh no, you lose.")  # noqa
+                print(Fore.RED + "You chose rock. The computer chose paper. Oh no, you lose.")  # noqa
                 self.comp_wins += 1
 
             elif self.computer_option == "s":
-                print(
-                    "You chose rock. The computer chose scissors. Woo hoo!! You win!")  # noqa
+                print(Fore.GREEN + "You chose rock. The computer chose scissors. Woo hoo!! You win!")  # noqa
                 self.player_wins += 1
 
         elif self.user_choice == "p":
 
             if self.computer_option == "r":
                 print(
-                    "You chose paper. The computer chose rock. Woo hoo!! You win!.")  # noqa
+                    Fore.GREEN + "You chose paper. The computer chose rock. Woo hoo!! You win!.")  # noqa
                 self.player_wins += 1
 
             elif self.computer_option == "p":
                 print(
-                    "You chose paper. The computer chose paper too. Congrats, you tied.")  # noqa
+                    Fore.YELLOW + "You chose paper. The computer chose paper too. Congrats, you tied.")  # noqa
 
             elif self.computer_option == "s":
                 print(
-                    "You chose paper. The computer chose scissors. Oh no, you lose.")  # noqa
+                    Fore.RED + "You chose paper. The computer chose scissors. Oh no, you lose.")  # noqa
                 self.comp_wins += 1
 
         elif self.user_choice == "s":
 
             if self.computer_option == "r":
                 print(
-                    "You chose scissors. The computer chose rock. Oh no, you lose.")  # noqa
+                    Fore.RED + "You chose scissors. The computer chose rock. Oh no, you lose.")  # noqa
                 self.comp_wins += 1
 
             elif self.computer_option == "p":
                 print(
-                    "You chose scissors. The computer chose paper. Woo hoo!! You win!.")  # noqa
+                    Fore.GREEN + "You chose scissors. The computer chose paper. Woo hoo!! You win!.")  # noqa
                 self.player_wins += 1
 
             elif self.computer_option == "s":
                 print(
-                    "You chose scissors. The computer chose scissors too. Congrats, you tied.")  # noqa
+                   Fore.YELLOW + "You chose scissors. The computer chose scissors too. Congrats, you tied.")  # noqa
 
     def new_round(self):
         self.user_choice = None
@@ -131,8 +134,8 @@ class Game:
         elif user_choice in ["n", "no"]:
             # record user playing game (add 1 to spreadsheet) create function.
 
-            print("Goodbye!" + self.myName)
-            print("Final Score")
+            print("Goodbye " + self.myName + "!\n")
+            print(Fore.MAGENTA + Back.WHITE + "Final Score")
             self.display_match_results()
             return False
         else:
@@ -154,7 +157,7 @@ def main():
 
     # pass in wins loses and games played if valid data
     game = Game(myName)
-    print("Hello, " + game.myName)
+    print(Fore.YELLOW + "Hello, " + game.myName)
     print("If this is your first time here, please check out our rules.")
     while True:
         user_choice = input(
@@ -166,8 +169,7 @@ def main():
         elif user_choice in ["start", "s", "y", "yes"]:
             return game.play()
         else:
-            print(
-                "Uh Oh, I don't think you've played this game before. Please try again.")  # noqa
+            print(Fore.RED + "Uh Oh, I don't think you've played this game before. Please try again.")  # noqa
 
 
 # if __name__ == 'main':
